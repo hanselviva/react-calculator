b115502fd8def46446081f1aae7d4d23b649b40d
HanselV <hanselviva@gmail.com>
2021-05-24 16:42:58 -0700

    added memory functions buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,35 +5,36 @@ const Buttons = () => {
 	return (
 		<div className="buttons-wrapper">
 			<table>
-				<tr>
-					<th className="button">M+</th>
-					<th className="button">MR</th>
-					<th className="button">MC</th>
+				<tr className="memory-functions">
+					<th>M+</th>
+					<th>MR</th>
+					<th>MC</th>
+					<th>CE</th>
 				</tr>
 				<tr>
-					<th className="button">1</th>
-					<th className="button">2</th>
-					<th className="button">3</th>
+					<th>7</th>
+					<th>8</th>
+					<th>9</th>
+					<th className="operator">X</th>
 				</tr>
 				<tr>
-					<th className="button">4</th>
-					<th className="button">5</th>
-					<th className="button">6</th>
+					<th>4</th>
+					<th>5</th>
+					<th>6</th>
+					<th className="operator">-</th>
 				</tr>
 				<tr>
-					<th className="button">7</th>
-					<th className="button">8</th>
-					<th className="button">0</th>
+					<th>1</th>
+					<th>2</th>
+					<th>3</th>
+					<th rowspan="2" className="operator">
+						+
+					</th>
 				</tr>
 				<tr>
-					<th className="button">0</th>
-					<th className="button">-</th>
-					<th className="button">+</th>
-				</tr>
-				<tr>
-					<th className="button">CE</th>
-					<th className="button">/</th>
-					<th className="button">*</th>
+					<th>0</th>
+					<th className="operator">/</th>
+					<th className="operator">=</th>
 				</tr>
 			</table>
 		</div>
